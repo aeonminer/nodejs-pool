4f314510836119c06c2601384114ca8629e951b6
aeonminer <34751669+aeonminer@users.noreply.github.com>
2017-12-21 14:49:22 -0600

    Update deploy.bash
    Install without caddy and monero daemon

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -22,22 +22,22 @@ sudo cmake .
 sudo make
 sudo mv libg* /usr/lib/
 cd ~
-sudo systemctl enable ntp
-cd /usr/local/src
-sudo git clone https://github.com/monero-project/monero.git
-cd monero
-sudo git checkout v0.11.1.0
-curl https://raw.githubusercontent.com/Snipa22/nodejs-pool/master/deployment/monero_daemon.patch | sudo git apply -v
-sudo make -j$(nproc)
-sudo cp ~/nodejs-pool/deployment/monero.service /lib/systemd/system/
-sudo useradd -m monerodaemon -d /home/monerodaemon
-BLOCKCHAIN_DOWNLOAD_DIR=$(sudo -u monerodaemon mktemp -d)
-sudo -u monerodaemon wget --limit-rate=50m -O $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw https://downloads.getmonero.org/blockchain.raw
-sudo -u monerodaemon /usr/local/src/monero/build/release/bin/monero-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --batch-size 20000 --database lmdb#fastest --verify off --data-dir /home/monerodaemon/.bitmonero
-sudo -u monerodaemon rm -rf $BLOCKCHAIN_DOWNLOAD_DIR
-sudo systemctl daemon-reload
-sudo systemctl enable monero
-sudo systemctl start monero
+#sudo systemctl enable ntp
+#cd /usr/local/src
+#sudo git clone https://github.com/monero-project/monero.git
+#cd monero
+#sudo git checkout v0.11.1.0
+#curl https://raw.githubusercontent.com/Snipa22/nodejs-pool/master/deployment/monero_daemon.patch | sudo git apply -v
+#sudo make -j$(nproc)
+#sudo cp ~/nodejs-pool/deployment/monero.service /lib/systemd/system/
+#sudo useradd -m monerodaemon -d /home/monerodaemon
+#BLOCKCHAIN_DOWNLOAD_DIR=$(sudo -u monerodaemon mktemp -d)
+#sudo -u monerodaemon wget --limit-rate=50m -O $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw https://downloads.getmonero.org/blockchain.raw
+#sudo -u monerodaemon /usr/local/src/monero/build/release/bin/monero-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --batch-size 20000 --database lmdb#fastest --verify off --data-dir /home/monerodaemon/.bitmonero
+#sudo -u monerodaemon rm -rf $BLOCKCHAIN_DOWNLOAD_DIR
+#sudo systemctl daemon-reload
+#sudo systemctl enable monero
+#sudo systemctl start monero
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install v8.9.3
@@ -48,38 +48,38 @@ openssl req -subj "/C=IT/ST=Pool/L=Daemon/O=Mining Pool/CN=mining.pool" -newkey 
 mkdir ~/pool_db/
 sed -r "s/(\"db_storage_path\": ).*/\1\"\/home\/$CURUSER\/pool_db\/\",/" config_example.json > config.json
 cd ~
-git clone https://github.com/mesh0000/poolui.git
-cd poolui
-npm install
-./node_modules/bower/bin/bower update
-./node_modules/gulp/bin/gulp.js build
-cd build
-sudo ln -s `pwd` /var/www
-CADDY_DOWNLOAD_DIR=$(mktemp -d)
-cd $CADDY_DOWNLOAD_DIR
-curl -sL "https://snipanet.com/caddy.tar.gz" | tar -xz caddy init/linux-systemd/caddy.service
-sudo mv caddy /usr/local/bin
-sudo chown root:root /usr/local/bin/caddy
-sudo chmod 755 /usr/local/bin/caddy
-sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
-sudo groupadd -g 33 www-data
-sudo useradd -g www-data --no-user-group --home-dir /var/www --no-create-home --shell /usr/sbin/nologin --system --uid 33 www-data
-sudo mkdir /etc/caddy
-sudo chown -R root:www-data /etc/caddy
-sudo mkdir /etc/ssl/caddy
-sudo chown -R www-data:root /etc/ssl/caddy
-sudo chmod 0770 /etc/ssl/caddy
-sudo cp ~/nodejs-pool/deployment/caddyfile /etc/caddy/Caddyfile
-sudo chown www-data:www-data /etc/caddy/Caddyfile
-sudo chmod 444 /etc/caddy/Caddyfile
-sudo sh -c "sed 's/ProtectHome=true/ProtectHome=false/' init/linux-systemd/caddy.service > /etc/systemd/system/caddy.service"
-sudo chown root:root /etc/systemd/system/caddy.service
-sudo chmod 644 /etc/systemd/system/caddy.service
-sudo systemctl daemon-reload
-sudo systemctl enable caddy.service
-sudo systemctl start caddy.service
-rm -rf $CADDY_DOWNLOAD_DIR
-cd ~
+#git clone https://github.com/mesh0000/poolui.git
+#cd poolui
+#npm install
+#./node_modules/bower/bin/bower update
+#./node_modules/gulp/bin/gulp.js build
+#cd build
+#sudo ln -s `pwd` /var/www
+#CADDY_DOWNLOAD_DIR=$(mktemp -d)
+#cd $CADDY_DOWNLOAD_DIR
+#curl -sL "https://snipanet.com/caddy.tar.gz" | tar -xz caddy init/linux-systemd/caddy.service
+#sudo mv caddy /usr/local/bin
+#sudo chown root:root /usr/local/bin/caddy
+#sudo chmod 755 /usr/local/bin/caddy
+#sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
+#sudo groupadd -g 33 www-data
+#sudo useradd -g www-data --no-user-group --home-dir /var/www --no-create-home --shell /usr/sbin/nologin --system --uid 33 www-data
+#sudo mkdir /etc/caddy
+#sudo chown -R root:www-data /etc/caddy
+#sudo mkdir /etc/ssl/caddy
+#sudo chown -R www-data:root /etc/ssl/caddy
+#sudo chmod 0770 /etc/ssl/caddy
+#sudo cp ~/nodejs-pool/deployment/caddyfile /etc/caddy/Caddyfile
+#sudo chown www-data:www-data /etc/caddy/Caddyfile
+#sudo chmod 444 /etc/caddy/Caddyfile
+#sudo sh -c "sed 's/ProtectHome=true/ProtectHome=false/' init/linux-systemd/caddy.service > /etc/systemd/system/caddy.service"
+#sudo chown root:root /etc/systemd/system/caddy.service
+#sudo chmod 644 /etc/systemd/system/caddy.service
+#sudo systemctl daemon-reload
+#sudo systemctl enable caddy.service
+#sudo systemctl start caddy.service
+#rm -rf $CADDY_DOWNLOAD_DIR
+#cd ~
 sudo env PATH=$PATH:`pwd`/.nvm/versions/node/v8.9.3/bin `pwd`/.nvm/versions/node/v8.9.3/lib/node_modules/pm2/bin/pm2 startup systemd -u $CURUSER --hp `pwd`
 cd ~/nodejs-pool
 sudo chown -R $CURUSER. ~/.pm2
